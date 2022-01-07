74cabc83567cf6cb9f731041745631248995c8a6
Yannick Béot <yannick.beot@sailpoint.com>
2022-01-07 18:23:33 +0100

    ♻ refactoring for better pipeline support

--- scripts/Get-IdentityNowApplication.ps1
+++ scripts/Get-IdentityNowApplication.ps1
@@ -6,7 +6,7 @@ Get IdentityNow Application(s).
 .DESCRIPTION
 Get IdentityNow Application(s).
 
-.PARAMETER appID
+.PARAMETER Id
 (optional) The Application ID of an IdentityNow Application.
 
 .PARAMETER org
@@ -17,7 +17,7 @@ Get-IdentityNowApplication -org $true
 Get-IdentityNowApplication 
 
 .EXAMPLE
-Get-IdentityNowApplication -appID 24184
+Get-IdentityNowApplication -Id 24184
 
 .LINK
 http://darrenjrobinson.com/sailpoint-identitynow
@@ -26,33 +26,39 @@ http://darrenjrobinson.com/sailpoint-identitynow
 
     [cmdletbinding(DefaultParameterSetName = "AppID")]
     param(
-        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ParameterSetName = "AppID")]
-        [Alias("Id")]
-        [string]$AppID,
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, ParameterSetName = "AppID")]
+        [Alias("AppID")]
+        [string]$Id,
         [Parameter(Mandatory = $false, ValueFromPipeline = $true, ParameterSetName = "Org")]
         [switch]$org 
     )
+    Begin {
 
-    $v3Token = Get-IdentityNowAuth | Test-IdentityNowToken
-    $utime = [int][double]::Parse((Get-Date -UFormat %s))
-    try {
-        if ($appID) {
-            $uri = "https://$($IdentityNowConfiguration.orgName).api.identitynow.com/cc/api/app/get/$($appID)?_dc=$($utime)"
-        }
-        else {
-            $uri = "https://$($IdentityNowConfiguration.orgName).api.identitynow.com/cc/api/app/list?&_dc=$($utime)"
-            if ($org.IsPresent) {
-                $uri = $uri | Set-HttpQueryString -Name "filter" -Value "org"
+        $v3Token = Get-IdentityNowAuth | Test-IdentityNowToken
+    }
+    Process {
+
+        $utime = [int][double]::Parse((Get-Date -UFormat %s))
+        try {
+            if ($Id) {
+                $uri = "https://$($IdentityNowConfiguration.orgName).api.identitynow.com/cc/api/app/get/$($Id)?_dc=$($utime)"
             }
+            else {
+                $uri = "https://$($IdentityNowConfiguration.orgName).api.identitynow.com/cc/api/app/list?&_dc=$($utime)"
+                if ($org.IsPresent) {
+                    $uri = $uri | Set-HttpQueryString -Name "filter" -Value "org"
+                }
+            }
+        
+            Invoke-RestMethod -Method Get -Uri $uri `
+                -Headers @{Authorization = "$($v3Token.token_type) $($v3Token.access_token)" } | `
+                ? { $_ } | % { $_.PSObject.TypeNames.Insert(0, "IdentityNow.ApplicationCC"); $_ }
         }
-
-        Invoke-RestMethod -Method Get -Uri $uri `
-            -Headers @{Authorization = "$($v3Token.token_type) $($v3Token.access_token)" } | `
-            ? { $_ } | % { $_.PSObject.TypeNames.Insert(0, "IdentityNow.ApplicationCC"); $_ }
+        catch {
+            Write-Error "Application doesn't exist. Check App ID. $($_)"
+            throw $_
+        }
+    
     }
-    catch {
-        Write-Error "Application doesn't exist. Check App ID. $($_)" 
-    }
-
 }
 
--- scripts/Invoke-IdentityNowSourceReset.ps1
+++ scripts/Invoke-IdentityNowSourceReset.ps1
@@ -6,7 +6,10 @@ function Invoke-IdentityNowSourceReset {
 .DESCRIPTION
     Reset an IdentityNow Source.
 
-.PARAMETER sourceID
+.PARAMETER Source
+    Source object as returned by Get-IdentityNowSource
+
+.PARAMETER SourceID
     (required) The ID of an IdentityNow Source. eg. 45678
 
 .EXAMPLE
@@ -20,112 +23,76 @@ function Invoke-IdentityNowSourceReset {
     Reset a Source but leave the entitlements
     Invoke-IdentityNowSourceReset -sourceID 12345 -skip accounts
 
+.EXAMPLE
+    Get-IdentityNowSource -name "Active Directory" | Invoke-IdentityNowSourceReset -Wait
+
+.EXAMPLE
+    Get-IdentityNowSource -name Linkedin | Invoke-IdentityNowSourceReset -Wait
+
 .LINK
     http://darrenjrobinson.com/sailpoint-identitynow
 
 #>
     [cmdletbinding()]
     param(
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-        [string]$sourceID,
-        [Parameter(Mandatory = $false, ValueFromPipeline = $true)]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName="source")]
+        $Source,
+        [Parameter(Mandatory = $true, ParameterSetName="ID")]
+        [int]$SourceID,
+
+        [Parameter(Mandatory = $false)]
         [ValidateSet("accounts", "entitlements", IgnoreCase = $true)]
-        [string]$skip        
+        [string]$Skip,
+
+        [switch]$Wait
     )
+    Begin {
+        $v3Token = Get-IdentityNowAuth | Test-IdentityNowToken
+        $headers = @{"Authorization" = "Bearer $($v3Token.access_token)" }
+    
+    }
+    Process {
+        if ($Source) {
+            $SourceID = $Source.connectorAttributes.cloudExternalId
+        }
+        Write-Verbose "SourceID=$SourceID"
 
-    $v3Token = Get-IdentityNowAuth | Test-IdentityNowToken
+        $url = "https://$($IdentityNowConfiguration.orgName).identitynow.com/api/source/reset/$sourceID"
+        if ($skip) {
+            $url = $url | Set-HttpQueryString -Name "skip" -Value $skip
+        }
 
-    if ($token) {
         try {
-            if ($skip) {
-                $reset = Invoke-RestMethod -Method POST -uri "https://$($IdentityNowConfiguration.orgName).identitynow.com/api/source/reset/$($sourceID)?skip=$($skip)" -Headers @{"Authorization" = "Bearer $($token.access_token)" }
-                return $reset 
+            $reset = Invoke-RestMethod -Method POST -uri $url -Headers $headers
+            if (-not $Wait.IsPresent) {
+                return $reset
             }
-            else {            
-                $reset = Invoke-RestMethod -Method POST -uri "https://$($IdentityNowConfiguration.orgName).identitynow.com/api/source/reset/$($sourceID)" -Headers @{"Authorization" = "Bearer $($token.access_token)" }
-                return $reset 
-            }
+            do {
+                Write-Verbose "Waiting..."
+                Start-Sleep -Seconds 5
+
+                $utime = [int][double]::Parse((Get-Date -UFormat %s))
+                $uri = $((Get-IdentityNowOrg).'Private Base API URI') + "/event/list"
+                $uri = $uri | Set-HttpQueryString -Name "_dc" -Value $utime | `
+                    Set-HttpQueryString -Name "page" -Value 1 | `
+                    Set-HttpQueryString -Name "start" -Value 0 | `
+                    Set-HttpQueryString -Name "limit" -Value 3 | `
+                    Set-HttpQueryString -Name "sort" -Value '[{"property":"timestamp","direction":"DESC"}]' | `
+                    Set-HttpQueryString -Name "filter" -Value "[{`"property`":`"objectType`",`"value`":`"source`"},{`"property`":`"objectId`",`"value`":`"$sourceID`"}]"
+                $tasks = (Invoke-WebRequest -Headers $headers -Uri $uri).Content | ConvertFrom-Json
+                $task = $tasks.items | Where-Object id -eq $reset.attributes.eventId
+                if (-not $task) {
+                    throw "Task not found"
+                }
+                if ($task.status -ne "PENDING") {
+                    return $task
+                }
+
+            } while ($true)
         }
         catch {
-            Write-Error "$($_)" 
+            Write-Error "Could not reset source $sourceId $($_)"
+            throw $_
         }
     }
-    else {
-        Write-Error "Authentication Failed. Check your v2 API credentials. $($_)"
-    } 
 }
-
-
-# SIG # Begin signature block
-# MIINSwYJKoZIhvcNAQcCoIINPDCCDTgCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUk+MK/PKsMKCgfUFuI6SWFr+z
-# 2OigggqNMIIFMDCCBBigAwIBAgIQBAkYG1/Vu2Z1U0O1b5VQCDANBgkqhkiG9w0B
-# AQsFADBlMQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYD
-# VQQLExB3d3cuZGlnaWNlcnQuY29tMSQwIgYDVQQDExtEaWdpQ2VydCBBc3N1cmVk
-# IElEIFJvb3QgQ0EwHhcNMTMxMDIyMTIwMDAwWhcNMjgxMDIyMTIwMDAwWjByMQsw
-# CQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3d3cu
-# ZGlnaWNlcnQuY29tMTEwLwYDVQQDEyhEaWdpQ2VydCBTSEEyIEFzc3VyZWQgSUQg
-# Q29kZSBTaWduaW5nIENBMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA
-# +NOzHH8OEa9ndwfTCzFJGc/Q+0WZsTrbRPV/5aid2zLXcep2nQUut4/6kkPApfmJ
-# 1DcZ17aq8JyGpdglrA55KDp+6dFn08b7KSfH03sjlOSRI5aQd4L5oYQjZhJUM1B0
-# sSgmuyRpwsJS8hRniolF1C2ho+mILCCVrhxKhwjfDPXiTWAYvqrEsq5wMWYzcT6s
-# cKKrzn/pfMuSoeU7MRzP6vIK5Fe7SrXpdOYr/mzLfnQ5Ng2Q7+S1TqSp6moKq4Tz
-# rGdOtcT3jNEgJSPrCGQ+UpbB8g8S9MWOD8Gi6CxR93O8vYWxYoNzQYIH5DiLanMg
-# 0A9kczyen6Yzqf0Z3yWT0QIDAQABo4IBzTCCAckwEgYDVR0TAQH/BAgwBgEB/wIB
-# ADAOBgNVHQ8BAf8EBAMCAYYwEwYDVR0lBAwwCgYIKwYBBQUHAwMweQYIKwYBBQUH
-# AQEEbTBrMCQGCCsGAQUFBzABhhhodHRwOi8vb2NzcC5kaWdpY2VydC5jb20wQwYI
-# KwYBBQUHMAKGN2h0dHA6Ly9jYWNlcnRzLmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEFz
-# c3VyZWRJRFJvb3RDQS5jcnQwgYEGA1UdHwR6MHgwOqA4oDaGNGh0dHA6Ly9jcmw0
-# LmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEFzc3VyZWRJRFJvb3RDQS5jcmwwOqA4oDaG
-# NGh0dHA6Ly9jcmwzLmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEFzc3VyZWRJRFJvb3RD
-# QS5jcmwwTwYDVR0gBEgwRjA4BgpghkgBhv1sAAIEMCowKAYIKwYBBQUHAgEWHGh0
-# dHBzOi8vd3d3LmRpZ2ljZXJ0LmNvbS9DUFMwCgYIYIZIAYb9bAMwHQYDVR0OBBYE
-# FFrEuXsqCqOl6nEDwGD5LfZldQ5YMB8GA1UdIwQYMBaAFEXroq/0ksuCMS1Ri6en
-# IZ3zbcgPMA0GCSqGSIb3DQEBCwUAA4IBAQA+7A1aJLPzItEVyCx8JSl2qB1dHC06
-# GsTvMGHXfgtg/cM9D8Svi/3vKt8gVTew4fbRknUPUbRupY5a4l4kgU4QpO4/cY5j
-# DhNLrddfRHnzNhQGivecRk5c/5CxGwcOkRX7uq+1UcKNJK4kxscnKqEpKBo6cSgC
-# PC6Ro8AlEeKcFEehemhor5unXCBc2XGxDI+7qPjFEmifz0DLQESlE/DmZAwlCEIy
-# sjaKJAL+L3J+HNdJRZboWR3p+nRka7LrZkPas7CM1ekN3fYBIM6ZMWM9CBoYs4Gb
-# T8aTEAb8B4H6i9r5gkn3Ym6hU/oSlBiFLpKR6mhsRDKyZqHnGKSaZFHvMIIFVTCC
-# BD2gAwIBAgIQDOzRdXezgbkTF+1Qo8ZgrzANBgkqhkiG9w0BAQsFADByMQswCQYD
-# VQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3d3cuZGln
-# aWNlcnQuY29tMTEwLwYDVQQDEyhEaWdpQ2VydCBTSEEyIEFzc3VyZWQgSUQgQ29k
-# ZSBTaWduaW5nIENBMB4XDTIwMDYxNDAwMDAwMFoXDTIzMDYxOTEyMDAwMFowgZEx
-# CzAJBgNVBAYTAkFVMRgwFgYDVQQIEw9OZXcgU291dGggV2FsZXMxFDASBgNVBAcT
-# C0NoZXJyeWJyb29rMRowGAYDVQQKExFEYXJyZW4gSiBSb2JpbnNvbjEaMBgGA1UE
-# CxMRRGFycmVuIEogUm9iaW5zb24xGjAYBgNVBAMTEURhcnJlbiBKIFJvYmluc29u
-# MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwj7PLmjkknFA0MIbRPwc
-# T1JwU/xUZ6UFMy6AUyltGEigMVGxFEXoVybjQXwI9hhpzDh2gdxL3W8V5dTXyzqN
-# 8LUXa6NODjIzh+egJf/fkXOgzWOPD5fToL7mm4JWofuaAwv2DmI2UtgvQGwRhkUx
-# Y3hh0+MNDSyz28cqExf8H6mTTcuafgu/Nt4A0ddjr1hYBHU4g51ZJ96YcRsvMZSu
-# 8qycBUNEp8/EZJxBUmqCp7mKi72jojkhu+6ujOPi2xgG8IWE6GqlmuMVhRSUvF7F
-# 9PreiwPtGim92RG9Rsn8kg1tkxX/1dUYbjOIgXOmE1FAo/QU6nKVioJMNpNsVEBz
-# /QIDAQABo4IBxTCCAcEwHwYDVR0jBBgwFoAUWsS5eyoKo6XqcQPAYPkt9mV1Dlgw
-# HQYDVR0OBBYEFOh6QLkkiXXHi1nqeGozeiSEHADoMA4GA1UdDwEB/wQEAwIHgDAT
-# BgNVHSUEDDAKBggrBgEFBQcDAzB3BgNVHR8EcDBuMDWgM6Axhi9odHRwOi8vY3Js
-# My5kaWdpY2VydC5jb20vc2hhMi1hc3N1cmVkLWNzLWcxLmNybDA1oDOgMYYvaHR0
-# cDovL2NybDQuZGlnaWNlcnQuY29tL3NoYTItYXNzdXJlZC1jcy1nMS5jcmwwTAYD
-# VR0gBEUwQzA3BglghkgBhv1sAwEwKjAoBggrBgEFBQcCARYcaHR0cHM6Ly93d3cu
-# ZGlnaWNlcnQuY29tL0NQUzAIBgZngQwBBAEwgYQGCCsGAQUFBwEBBHgwdjAkBggr
-# BgEFBQcwAYYYaHR0cDovL29jc3AuZGlnaWNlcnQuY29tME4GCCsGAQUFBzAChkJo
-# dHRwOi8vY2FjZXJ0cy5kaWdpY2VydC5jb20vRGlnaUNlcnRTSEEyQXNzdXJlZElE
-# Q29kZVNpZ25pbmdDQS5jcnQwDAYDVR0TAQH/BAIwADANBgkqhkiG9w0BAQsFAAOC
-# AQEANWoHDjN7Hg9QrOaZx0V8MK4c4nkYBeFDCYAyP/SqwYeAtKPA7F72mvmJV6E3
-# YZnilv8b+YvZpFTZrw98GtwCnuQjcIj3OZMfepQuwV1n3S6GO3o30xpKGu6h0d4L
-# rJkIbmVvi3RZr7U8ruHqnI4TgbYaCWKdwfLb/CUffaUsRX7BOguFRnYShwJmZAzI
-# mgBx2r2vWcZePlKH/k7kupUAWSY8PF8O+lvdwzVPSVDW+PoTqfI4q9au/0U77UN0
-# Fq/ohMyQ/CUX731xeC6Rb5TjlmDhdthFP3Iho1FX0GIu55Py5x84qW+Ou+OytQcA
-# FZx22DA8dAUbS3P7OIPamcU68TGCAigwggIkAgEBMIGGMHIxCzAJBgNVBAYTAlVT
-# MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j
-# b20xMTAvBgNVBAMTKERpZ2lDZXJ0IFNIQTIgQXNzdXJlZCBJRCBDb2RlIFNpZ25p
-# bmcgQ0ECEAzs0XV3s4G5ExftUKPGYK8wCQYFKw4DAhoFAKB4MBgGCisGAQQBgjcC
-# AQwxCjAIoAKAAKECgAAwGQYJKoZIhvcNAQkDMQwGCisGAQQBgjcCAQQwHAYKKwYB
-# BAGCNwIBCzEOMAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkEMRYEFHaZ/MVb8O28
-# o9F8CAKNM1u28ezMMA0GCSqGSIb3DQEBAQUABIIBAKU8yuc0PxHoNsBsp5miZjTO
-# KFOcxLc8y4ndPlRSjlPs6ca0jm9WIr6XhPRS69qPYZmpHHxAZckME5ZFpGuekDJY
-# ct4GHPBQG4eSbJ0Le+HVxlSOF4v8EePY5LW5UZOp79nieqUI9POdL6EdHSJTrQFK
-# p0B1v0XS3wj177ndWyEV8wHmSL/sTg8rJJy3+9769fjfnjTAAtbUUdJxH2gTwJdB
-# KvUA6SUnyvksMXvvCaRvGM/rRJ2SmAG1zUNoOqkZNXx8Z5cdTbGXGZ/7sGMrTC5P
-# v51m0KEe3QJF4mchESZw9mVUJ/cnt1M6NVyKYs1QjJ0fz83dh2XepoE6MBY59uU=
-# SIG # End signature block
