b9c90a23d4dd40accaca872c9121926242ebaf3a
Yannick Béot <yannick.beot@sailpoint.com>
2022-08-16 16:39:46 +0200

    ♻ Add test, optional params, etc. on Invoke-IdentityNowRequest

--- scripts/Invoke-IdentityNowRequest.ps1
+++ scripts/Invoke-IdentityNowRequest.ps1
@@ -58,165 +58,108 @@ http://darrenjrobinson.com/sailpoint-identitynow
 
     [cmdletbinding()]
     param(
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = 'Full URL')]
+        [Parameter(Mandatory = $true, ParameterSetName = 'Full URL')]
         [string]$uri,
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = 'Path')]
-        [string]$path,
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = 'Path')]
-        [string][ValidateSet("V1", "V2", "V3", "Private", "Beta")]$API,
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        
+        [Parameter(Mandatory = $true, ParameterSetName = 'Path')]
+        [string[]]$path,
+        [Parameter(Mandatory = $true, ParameterSetName = 'Path')]
+        
+        [string][ValidateSet("V1", "V2", "V3", "Private", "Beta", "CC")]$API,
+        
+        [Parameter(Mandatory = $true)]
         [ValidateNotNullOrEmpty()]
-        [string][ValidateSet("Get", "Put", "Patch", "Delete", "Post")]$method,
-        [ValidateNotNullOrEmpty()]
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-        [string][ValidateSet("HeadersV2", "HeadersV3", "Headersv2_JSON", "Headersv3_JSON", "Headersv3_JSON-Patch")]$headers,
-        [Parameter(Mandatory = $false, ValueFromPipeline = $true)]
-        [string]$body,
-        [Parameter(Mandatory = $false, ValueFromPipeline = $true)]
+        [string][ValidateSet("Get", "Put", "Patch", "Delete", "Post")]$Method,
+
+        [ValidateSet("HeadersV2", "HeadersV3", "Headersv2_JSON", "Headersv3_JSON", "Headersv3_JSON-Patch")]
+        [string]
+        $headers,
+        
+        $body,
+        
         [switch]$json
     )
 
-    switch ($API) {
-        "Private" { $uri = "$((Get-IdentityNowOrg).'Private Base API URI')/$path" }
-        "V1" { $uri = "$((Get-IdentityNowOrg).'v1 Base API URI')/$path" }
-        "V2" { $uri = "$((Get-IdentityNowOrg).'v2 Base API URI')/$path" }
-        "V3" { $uri = "$((Get-IdentityNowOrg).'v3 Base API URI')/$path" }
-        "Beta" { $uri = "$((Get-IdentityNowOrg).'Beta')/$path" }
+    Write-Verbose "> Invoke-IdentityNowRequest"
+    Write-Verbose "PSCmdlet.ParameterSetName=$($PSCmdlet.ParameterSetName)"
+
+    if ($PSCmdlet.ParameterSetName -eq "Path") {
+        # For compatibility
+        if ("Private" -eq $API) { $API = "cc" }
+        $uri = Get-IdentityNowOrgUrl -endpoint $API -parts $path
+    }
+
+    if (-not $headers) {
+        if ("Get" -eq $method) {
+            if ("v2" -eq $API) {
+                $headers = "HeadersV2"
+            }
+            else {
+                $headers = "HeadersV3"
+            }
+        }
+        else {
+            if ("v2" -eq $API) {
+                $headers = "Headersv2_JSON"
+            }
+            else {
+                $headers = "Headersv3_JSON"
+            }
+        }
     }
     switch ($headers) {
         HeadersV2 { 
             $requestHeaders = Get-IdentityNowAuth -return V2Header
-            Write-Verbose "$requestheaders"
         }
         HeadersV3 { 
             $v3Token = Get-IdentityNowAuth -return V3JWT
             $requestHeaders = @{Authorization = "Bearer $($v3Token.access_token)" }
-            Write-Verbose "Authorization = Bearer $($v3Token.access_token)"
         }
         Headersv2_JSON { 
             $requestHeaders = Get-IdentityNowAuth -return V2Header
             $requestHeaders.'Content-Type' = "application/json" 
-            Write-Verbose "Authorization = 'Basic $($encodedAuth)' ; 'Content-Type' = 'application/json' "
         }
         Headersv3_JSON { 
             $v3Token = Get-IdentityNowAuth  -return V3JWT
             $requestHeaders = @{Authorization = "Bearer $($v3Token.access_token)"; "Content-Type" = "application/json" }
-            Write-Verbose "Authorization = 'Bearer $($v3Token.access_token)' ; 'Content-Type' = 'application/json'"
-            Write-verbose ($v3Token | convertTo-json)
         }
         Headersv3_JSON-Patch { 
             $v3Token = Get-IdentityNowAuth -return V3JWT
             $requestHeaders = @{Authorization = "Bearer $($v3Token.access_token)"; "Content-Type" = "application/json-patch+json" }
-            Write-Verbose "Authorization = 'Bearer $($v3Token.access_token)'; 'Content-Type' = 'application/json-patch+json'"
-            Write-verbose ($v3Token | convertTo-json)
         }
         default { 
             $requestHeaders = $headers 
         } 
     }
     
-    Write-Verbose $requestHeaders
-    
-    if ($requestHeaders) {
-        try {
-            if ($body) {
-                if ($json) {
-                    $result = (Invoke-WebRequest -Method $method -Uri $uri -Headers $requestHeaders -Body $body).content
-                }
-                else {
-                    $result = Invoke-RestMethod -Method $method -Uri $uri -Headers $requestHeaders -Body $body 
-                }
-            }
-            else {   
-                if ($json) {
-                    $result = (Invoke-WebRequest -Method $method -Uri $uri -Headers $requestHeaders).content
-                }
-                else {      
-                    $result = Invoke-RestMethod -Method $method -Uri $uri -Headers $requestHeaders        
-                }
-            }
-            return $result
+    #Write-Verbose "requestHeaders = $($requestHeaders | Out-String)"
+    Write-Verbose "Uri = $uri"
+    $requestHeaders | Test-IdentityNowToken | Out-Null
+
+    $argSplat = @{
+        Method  = $method
+        Uri     = $uri
+        Headers = $requestHeaders
+    }
+
+    if ($body) {
+        if ($body -isnot [string]) {
+            $body = ConvertTo-Json -Depth 100 $body
         }
-        catch {
-            Write-Error "Request Failed. Check your request parameters. $($_)" 
+        $argSplat.add("Body", $body)
+    }
+    try {
+        $result = Invoke-WebRequest @argSplat
+        Write-Verbose "< Invoke-IdentityNowRequest"
+        if ($json.IsPresent) {
+            return $result.content | ConvertFrom-Json
+        }
+        else {
+            return $result.content
         }
     }
-    else {
-        Write-Error "No Request Headers computed. Check your request `$headers parameter. $($_)"
-        return $v3Token
-    } 
+    catch {
+        Write-Error "Request Failed. Check your request parameters. $($_)"
+        throw $_
+    }
 }
-
-# SIG # Begin signature block
-# MIINSwYJKoZIhvcNAQcCoIINPDCCDTgCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUre26RlIhGtKrnenG3cd5GBSb
-# HbegggqNMIIFMDCCBBigAwIBAgIQBAkYG1/Vu2Z1U0O1b5VQCDANBgkqhkiG9w0B
-# AQsFADBlMQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYD
-# VQQLExB3d3cuZGlnaWNlcnQuY29tMSQwIgYDVQQDExtEaWdpQ2VydCBBc3N1cmVk
-# IElEIFJvb3QgQ0EwHhcNMTMxMDIyMTIwMDAwWhcNMjgxMDIyMTIwMDAwWjByMQsw
-# CQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3d3cu
-# ZGlnaWNlcnQuY29tMTEwLwYDVQQDEyhEaWdpQ2VydCBTSEEyIEFzc3VyZWQgSUQg
-# Q29kZSBTaWduaW5nIENBMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA
-# +NOzHH8OEa9ndwfTCzFJGc/Q+0WZsTrbRPV/5aid2zLXcep2nQUut4/6kkPApfmJ
-# 1DcZ17aq8JyGpdglrA55KDp+6dFn08b7KSfH03sjlOSRI5aQd4L5oYQjZhJUM1B0
-# sSgmuyRpwsJS8hRniolF1C2ho+mILCCVrhxKhwjfDPXiTWAYvqrEsq5wMWYzcT6s
-# cKKrzn/pfMuSoeU7MRzP6vIK5Fe7SrXpdOYr/mzLfnQ5Ng2Q7+S1TqSp6moKq4Tz
-# rGdOtcT3jNEgJSPrCGQ+UpbB8g8S9MWOD8Gi6CxR93O8vYWxYoNzQYIH5DiLanMg
-# 0A9kczyen6Yzqf0Z3yWT0QIDAQABo4IBzTCCAckwEgYDVR0TAQH/BAgwBgEB/wIB
-# ADAOBgNVHQ8BAf8EBAMCAYYwEwYDVR0lBAwwCgYIKwYBBQUHAwMweQYIKwYBBQUH
-# AQEEbTBrMCQGCCsGAQUFBzABhhhodHRwOi8vb2NzcC5kaWdpY2VydC5jb20wQwYI
-# KwYBBQUHMAKGN2h0dHA6Ly9jYWNlcnRzLmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEFz
-# c3VyZWRJRFJvb3RDQS5jcnQwgYEGA1UdHwR6MHgwOqA4oDaGNGh0dHA6Ly9jcmw0
-# LmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEFzc3VyZWRJRFJvb3RDQS5jcmwwOqA4oDaG
-# NGh0dHA6Ly9jcmwzLmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEFzc3VyZWRJRFJvb3RD
-# QS5jcmwwTwYDVR0gBEgwRjA4BgpghkgBhv1sAAIEMCowKAYIKwYBBQUHAgEWHGh0
-# dHBzOi8vd3d3LmRpZ2ljZXJ0LmNvbS9DUFMwCgYIYIZIAYb9bAMwHQYDVR0OBBYE
-# FFrEuXsqCqOl6nEDwGD5LfZldQ5YMB8GA1UdIwQYMBaAFEXroq/0ksuCMS1Ri6en
-# IZ3zbcgPMA0GCSqGSIb3DQEBCwUAA4IBAQA+7A1aJLPzItEVyCx8JSl2qB1dHC06
-# GsTvMGHXfgtg/cM9D8Svi/3vKt8gVTew4fbRknUPUbRupY5a4l4kgU4QpO4/cY5j
-# DhNLrddfRHnzNhQGivecRk5c/5CxGwcOkRX7uq+1UcKNJK4kxscnKqEpKBo6cSgC
-# PC6Ro8AlEeKcFEehemhor5unXCBc2XGxDI+7qPjFEmifz0DLQESlE/DmZAwlCEIy
-# sjaKJAL+L3J+HNdJRZboWR3p+nRka7LrZkPas7CM1ekN3fYBIM6ZMWM9CBoYs4Gb
-# T8aTEAb8B4H6i9r5gkn3Ym6hU/oSlBiFLpKR6mhsRDKyZqHnGKSaZFHvMIIFVTCC
-# BD2gAwIBAgIQDOzRdXezgbkTF+1Qo8ZgrzANBgkqhkiG9w0BAQsFADByMQswCQYD
-# VQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3d3cuZGln
-# aWNlcnQuY29tMTEwLwYDVQQDEyhEaWdpQ2VydCBTSEEyIEFzc3VyZWQgSUQgQ29k
-# ZSBTaWduaW5nIENBMB4XDTIwMDYxNDAwMDAwMFoXDTIzMDYxOTEyMDAwMFowgZEx
-# CzAJBgNVBAYTAkFVMRgwFgYDVQQIEw9OZXcgU291dGggV2FsZXMxFDASBgNVBAcT
-# C0NoZXJyeWJyb29rMRowGAYDVQQKExFEYXJyZW4gSiBSb2JpbnNvbjEaMBgGA1UE
-# CxMRRGFycmVuIEogUm9iaW5zb24xGjAYBgNVBAMTEURhcnJlbiBKIFJvYmluc29u
-# MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwj7PLmjkknFA0MIbRPwc
-# T1JwU/xUZ6UFMy6AUyltGEigMVGxFEXoVybjQXwI9hhpzDh2gdxL3W8V5dTXyzqN
-# 8LUXa6NODjIzh+egJf/fkXOgzWOPD5fToL7mm4JWofuaAwv2DmI2UtgvQGwRhkUx
-# Y3hh0+MNDSyz28cqExf8H6mTTcuafgu/Nt4A0ddjr1hYBHU4g51ZJ96YcRsvMZSu
-# 8qycBUNEp8/EZJxBUmqCp7mKi72jojkhu+6ujOPi2xgG8IWE6GqlmuMVhRSUvF7F
-# 9PreiwPtGim92RG9Rsn8kg1tkxX/1dUYbjOIgXOmE1FAo/QU6nKVioJMNpNsVEBz
-# /QIDAQABo4IBxTCCAcEwHwYDVR0jBBgwFoAUWsS5eyoKo6XqcQPAYPkt9mV1Dlgw
-# HQYDVR0OBBYEFOh6QLkkiXXHi1nqeGozeiSEHADoMA4GA1UdDwEB/wQEAwIHgDAT
-# BgNVHSUEDDAKBggrBgEFBQcDAzB3BgNVHR8EcDBuMDWgM6Axhi9odHRwOi8vY3Js
-# My5kaWdpY2VydC5jb20vc2hhMi1hc3N1cmVkLWNzLWcxLmNybDA1oDOgMYYvaHR0
-# cDovL2NybDQuZGlnaWNlcnQuY29tL3NoYTItYXNzdXJlZC1jcy1nMS5jcmwwTAYD
-# VR0gBEUwQzA3BglghkgBhv1sAwEwKjAoBggrBgEFBQcCARYcaHR0cHM6Ly93d3cu
-# ZGlnaWNlcnQuY29tL0NQUzAIBgZngQwBBAEwgYQGCCsGAQUFBwEBBHgwdjAkBggr
-# BgEFBQcwAYYYaHR0cDovL29jc3AuZGlnaWNlcnQuY29tME4GCCsGAQUFBzAChkJo
-# dHRwOi8vY2FjZXJ0cy5kaWdpY2VydC5jb20vRGlnaUNlcnRTSEEyQXNzdXJlZElE
-# Q29kZVNpZ25pbmdDQS5jcnQwDAYDVR0TAQH/BAIwADANBgkqhkiG9w0BAQsFAAOC
-# AQEANWoHDjN7Hg9QrOaZx0V8MK4c4nkYBeFDCYAyP/SqwYeAtKPA7F72mvmJV6E3
-# YZnilv8b+YvZpFTZrw98GtwCnuQjcIj3OZMfepQuwV1n3S6GO3o30xpKGu6h0d4L
-# rJkIbmVvi3RZr7U8ruHqnI4TgbYaCWKdwfLb/CUffaUsRX7BOguFRnYShwJmZAzI
-# mgBx2r2vWcZePlKH/k7kupUAWSY8PF8O+lvdwzVPSVDW+PoTqfI4q9au/0U77UN0
-# Fq/ohMyQ/CUX731xeC6Rb5TjlmDhdthFP3Iho1FX0GIu55Py5x84qW+Ou+OytQcA
-# FZx22DA8dAUbS3P7OIPamcU68TGCAigwggIkAgEBMIGGMHIxCzAJBgNVBAYTAlVT
-# MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j
-# b20xMTAvBgNVBAMTKERpZ2lDZXJ0IFNIQTIgQXNzdXJlZCBJRCBDb2RlIFNpZ25p
-# bmcgQ0ECEAzs0XV3s4G5ExftUKPGYK8wCQYFKw4DAhoFAKB4MBgGCisGAQQBgjcC
-# AQwxCjAIoAKAAKECgAAwGQYJKoZIhvcNAQkDMQwGCisGAQQBgjcCAQQwHAYKKwYB
-# BAGCNwIBCzEOMAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkEMRYEFMyfsVUK3Ayl
-# ik2Y+CctWUuyLYpeMA0GCSqGSIb3DQEBAQUABIIBABOMdxfRejxB4++zBr5jLAdK
-# pybC8wnzS/vX1+CXO+RqcxYi54PQM7JUNcslQd7DMHmj7l17ifoV+tS8DlAJwm11
-# TaAYs6efQ+FCe6uE8AWFU/8Gn7H6Q1wbw8qQZ3Y8xsnORCzEdXZEs0onsLFj4Jb9
-# 4XuMt62XNSvklK10n68KViD8ZOj4mLCF1w9ybIXjPPGmE7l6f13xYYZV2FU2sTHV
-# VUcif+DHjTK+/NOtA1iSBad+O84Z9WLIHEUkoDBu6KjKPQr6uTVcH6uYZFOCSh7s
-# I/aK14pqhTROWv9p/pBHdhXmevWQ5iOXM7T+ga6wHPepJV8XWodjMy+WWLAFn84=
-# SIG # End signature block
