120d2e72aca9adb6b3c283b70e4f2db9afe24c2e
Yannick Béot <yannick.beot@sailpoint.com>
2022-09-13 13:06:02 +0200

    🐞♻ call to Get-IdentityNowOrgUrl was not working => use of Invoke-IdentityNowRequest

--- scripts/Search-IdentityNowUserProfile.ps1
+++ scripts/Search-IdentityNowUserProfile.ps1
@@ -34,21 +34,15 @@ http://darrenjrobinson.com/sailpoint-identitynow
         [string]$limit = 250
     )
 
-    $v3Token = Get-IdentityNowAuth
 
-    if ($v3Token.access_token) {
-        try {                         
-            # Get User Profiles Based on Query
-            $userProfiles = Invoke-RestMethod -Method Get -Uri (Get-IdentityNowOrgUrl cc"/user/list?_dc=$($utime)&listErrorFirst=true&useSds=true&start=0&limit=$($limit)&sorters=%5B%7B%22property%22%3A%22name%22%2C%22direction%22%3A%22ASC%22%7D%5D&filters=%5B%7B%22property%22%3A%22username%22%2C%22value%22%3A%22$($query)%22%7D%5D") -Headers @{Authorization = "$($v3Token.token_type) $($v3Token.access_token)" } 
-            return $userProfiles.items
-        }
-        catch {
-            Write-Error "Bad Query. Check your query. $($_)" 
-        }
+    try {    
+        $utime = [int][double]::Parse((Get-Date -UFormat %s))                     
+        # Get User Profiles Based on Query
+        $userProfiles = Invoke-IdentityNowRequest -API CC -path "/user/list?_dc=$($utime)&listErrorFirst=true&useSds=true&start=0&limit=$($limit)&sorters=%5B%7B%22property%22%3A%22name%22%2C%22direction%22%3A%22ASC%22%7D%5D&filters=%5B%7B%22property%22%3A%22username%22%2C%22value%22%3A%22$($query)%22%7D%5D" -json
+        return $userProfiles.items
     }
-    else {
-        Write-Error "Authentication Failed. Check your AdminCredential and v3 API ClientID and ClientSecret. $($_)"
-        return $v3Token
-    } 
+    catch {
+        Write-Error "Bad Query. Check your query. $($_)" 
+    }
 }
 
